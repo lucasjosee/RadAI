c64d3391485f3841ef14ba4d304bf1eb22f3e9f8
Lucas José Souza Rodrigues <lucas.souza742007@gmail.com>
2026-05-03 18:52:07 -0300

    fix: remove hardcoded database credentials from Conexao.java

--- Codigo/backend/src/main/java/dao/Conexao.java
+++ Codigo/backend/src/main/java/dao/Conexao.java
@@ -8,17 +8,10 @@ public class Conexao {
 
     public static Sql2o getSql2o() {
         if (sql2o == null) {
-            // Tenta pegar as configurações da Nuvem
             String dbUrl = System.getenv("DB_URL");
             String dbUser = System.getenv("DB_USER");
             String dbPass = System.getenv("DB_PASS");
-
-            if (dbUrl != null) {
-
-                sql2o = new Sql2o(dbUrl, dbUser, dbPass);
-            } else {
-                sql2o = new Sql2o("jdbc:postgresql://localhost:5432/saas_raiox", "postgres", "123456");
-            }
+            sql2o = new Sql2o(dbUrl, dbUser, dbPass);
         }
         return sql2o;
     }
